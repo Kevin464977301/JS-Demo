954943925913d6863fbf5026861e8e7f0a0b5f16
秦凯华 <464977301@qq.com>
2017-08-23 19:58:14 +0800

    JavaScript自行练习

---
+++ 8.08/names.js
@@ -0,0 +1,14 @@
+var name="姓名:凯华";
+var sex="性别:男";
+var age="年龄:20";
+var height="身高:170cm";
+myname=[name,"<br/>"+sex,"<br/>"+age,"<br/>"+height];
+document.write(myname+"<br/>");
+
+document.write(name+"&nbsp;");
+document.write(sex+"&nbsp;");
+document.write(age+"&nbsp;");
+document.write(height+"<br/>");
+var bard="<a href='http://www.baidu.com'>"+name+"链接百度"+"</a>"
+document.write(bard);
+
---
+++ 8.09/zuoye.js
@@ -0,0 +1,40 @@
+//1 var a = prompt("请输入内容");
+//var b = a||"主人很懒";
+//alert(b);
+
+//2 var c = prompt("猜一猜数字是0-10中的哪个?");
+//var d = 9;
+//var e = (c==d);
+//alert(e);
+//if(c!=d){
+//	alert("再接再厉");
+//}else{
+//	alert("恭喜猜对了");
+//}
+//3 var f=prompt("请输入汉字");
+//var g="\u4e00";
+//var h="\u9fa5";
+//console.log(f>=g || f<=h);
+//console.log(g.charAt());
+//console.log(h.charAt());
+
+//4 var i = prompt("输入你要写的字,我来判断");
+//console.log(i>="a" && i<="z");
+
+var x=5,y=7;
+//var z;
+//z=x;
+//x=y;
+//y=z;
+//console.log(x,y);
+//x=y-x;
+//y=y-x;
+//x=x+y;
+//console.log(x,y);
+//x+=(y-=x);
+//y=x-y;
+//console.log(x,y);
+
+//console.log((y-=x));
+//console.log(y);
+//console.log(x);
